f3dddd405f84d9db5f9d81a438b7febde4eb3ef1
David Clark <dlc.clark@gmail.com>
2018-01-14 05:29:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artist, song){
-  Phil Ochs: "Here's to the State of Mississipi";
+  playlist.Phil Ochs: "Here's to the State of Mississipi";
   return playlist;
 }
