e8995fdf052ba13789194a56ed914cb96f841d59
David Clark <dlc.clark@gmail.com>
2018-01-14 05:47:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete.Kanye: "Gold Digger"
+  delete["Kanye"] = "Gold Digger";
   return playlist;
 }
