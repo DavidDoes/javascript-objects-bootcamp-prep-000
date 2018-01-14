08745e4e90d6af5a956dad6f9aa44bf84c5e9d23
David Clark <dlc.clark@gmail.com>
2018-01-14 05:47:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete['Phil Ochs'];
+  delete.Kanye: "Gold Digger"
   return playlist;
 }
